d8fb7784cc594d98bbcd13d46caf7ece177d5486
Thorben Buchta <thorbenbuchta@gmail.com>
2020-01-20 20:46:49 +0100

    feat(data-access): add models for ingredients

---
+++ DataAccess/Models/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Models
+{
+    public class Ingredient
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? EAN { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
---
+++ DataAccess/Models/IngredientMeasure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Models
+{
+    public class IngredientMeasure
+    {
+        public double Amount { get; set; }
+        public string Unit { get; set; }
+    }
+}
